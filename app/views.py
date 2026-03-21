884fccd559c07d511ea0ca3a65e1bdde53f22891
joaofreitas77 <joaozinpr777@gmail.com>
2026-03-21 14:54:53 -0300

    adicionando template html, mostrando na tela a opcao de converter

--- app/views.py
+++ app/views.py
@@ -1,24 +1,31 @@
-from django.http import JsonResponse
+from django.shortcuts import render
 from .models import HistoricoConversao
 from .services import converter_moeda
 
 def converter_view(request):
     origem = request.GET.get('origem')
     destino = request.GET.get('destino')
-    valor = float(request.GET.get('valor'))
+    valor = request.GET.get('valor')
     
-    valor_convertido = converter_moeda(origem, destino, valor)
+    envia_dados = {}
     
-    HistoricoConversao.objects.create(
-        moeda_origem=origem,
-        moeda_destino=destino,
-        valor_original=valor,
-        valor_convertido=valor_convertido
-    )
+    if origem and destino and valor:
+        valor = float(valor)
     
-    return JsonResponse({
-        "origem": origem,
-        "destino": destino,
-        "valor_original": valor,
-        "valor_convertido": valor_convertido
-    })
+        valor_convertido = converter_moeda(origem, destino, valor)
+    
+        HistoricoConversao.objects.create(
+            moeda_origem=origem,
+            moeda_destino=destino,
+            valor_original=valor,
+            valor_convertido=valor_convertido
+        )
+    
+        envia_dados = {
+            "origem": origem,
+            "destino": destino,
+            "valor_original": valor,
+            "valor_convertido": valor_convertido
+        }
+    
+    return render(request, "converter.html", envia_dados)
