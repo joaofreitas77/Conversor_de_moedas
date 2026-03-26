bf5ce86068477174229585611408f7c5502842e4
joaofreitas77 <joaozinpr777@gmail.com>
2026-03-26 15:59:08 -0300

    adicionando mudanças

--- app/services.py
+++ app/services.py
@@ -2,9 +2,11 @@ def converter_moeda(origem, destino, valor):
     import requests
 
     url = f"https://api.frankfurter.dev/v1/latest?base={origem}&symbols={destino}"
+    #https://api.frankfurter.dev/v1/latest?base=35.85&symbols=ARS
     
     response = requests.get(url)
     data = response.json()
+    print(data)
 
     taxa = data['rates'][destino]
 
