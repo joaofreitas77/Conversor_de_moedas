c1e36659a8fd994f917560461d898dbd877b340e
joaofreitas77 <joaozinpr777@gmail.com>
2026-03-26 18:48:55 -0300

    mudanças

--- app/services.py
+++ app/services.py
@@ -2,7 +2,6 @@ def converter_moeda(origem, destino, valor):
     import requests
 
     url = f"https://api.frankfurter.dev/v1/latest?base={origem}&symbols={destino}"
-    #https://api.frankfurter.dev/v1/latest?base=35.85&symbols=ARS
     
     response = requests.get(url)
     data = response.json()
